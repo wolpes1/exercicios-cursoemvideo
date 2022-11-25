23f8c70a23d8340a0ddfe313217c980d9e46b237
Renan Lopes Cavalcante <renanlc1857@gmail.com>
2022-11-24 21:10:11 -0300

    Exercícios novos

---
+++ exercicio-2.py
@@ -0,0 +1,3 @@
+nome = input('Qual é seu nome?\n')
+
+print(f'Olá, {nome}! Tudo bem com você?')
---
+++ exercicio-3.py
@@ -0,0 +1,4 @@
+n1 = int(input('Qual o primeiro número a ser somado?\n'))
+n2 = int(input('E o segundo número?\n'))
+
+print(f'A soma de {n1} e {n2} é {(n1+n2)}')
